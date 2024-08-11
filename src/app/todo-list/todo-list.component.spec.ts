9ed5a901e58cdaa10aff96045bedfdaa3f7f7b1d
amandanalesso <nalessoamanda@gmail.com>
2024-08-10 23:28:05 -0300

    a9 usando angular

---
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  newTask: string = '';
+  tasks: string[] = [];
+
+  addTask(): void {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  removeTask(task: string): void {
+    this.tasks = this.tasks.filter(t => t !== task);
+  }
+}
